f8397b0a18c51417233fb0f4393e0d6740986387
kyleherget <111830966+kyleherget@users.noreply.github.com>
2022-09-27 00:49:34 -0400

    Update app.js

---
+++ OverandOut/lab13/js/app.js
@@ -0,0 +1,6 @@
+    let width = 100 // setting width var
+    let height = 100 // setting height var
+
+    //start 
+    dvMydiv.style.backgroundColor = "#0000cc";
+    let div = document.getElementById('mydiv') // getting element by ID
