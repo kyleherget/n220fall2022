2abb5828abb8d02b87485cecc0623d23f8c98971
kyleherget <111830966+kyleherget@users.noreply.github.com>
2022-09-27 17:04:53 -0400

    update

--- OverandOut/lab13/js/app.js
+++ OverandOut/lab13/js/app.js
@@ -2,5 +2,8 @@
     let height = 100 // setting height var
 
     //start 
-    dvMydiv.style.backgroundColor = "#0000cc";
+    div.style.backgroundColor = "#0000cc"; // the color blue 
     let div = document.getElementById('mydiv') // getting element by ID
+    div.addEventListener("mouseOver",(mouse) => { // uses the eevent listneer to change the color to black when the mouse is over the box
+        mouse.target.style.color = "#000000"; // the color black 
+    })
