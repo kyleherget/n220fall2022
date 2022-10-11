82573cc124dbe6ee3729facc1ccd6ae5839bf29d
kyleherget <111830966+kyleherget@users.noreply.github.com>
2022-10-11 15:45:16 -0400

    updates

--- BeepBop/lab15/js/app.js
+++ BeepBop/lab15/js/app.js
@@ -1,7 +1,7 @@
 // Kyle Herget 10/4/22 NEWM-N 220
 
 //start
-for(var i = 1; i <= 25; i++) { //loop for beeps and bops
+for(var i = 0; i <= 25; i++) { //loop for beeps and bops
     if(i % 3 == 0) // if divisible by 3 print to the console ("beep") 
     console.log("beep") // logs beep
     else if(i % 5 == 0) // if divisible by 5 print to the console ("bop")
