27623f50b5ae73a0bfcab8d959e4e48a695af6e4
kyleherget <111830966+kyleherget@users.noreply.github.com>
2022-10-26 20:58:38 -0400

    refine

--- TipCalculator/lab22/js/app.js
+++ TipCalculator/lab22/js/app.js
@@ -2,11 +2,11 @@
 
 //start
 
-function Tip() {
+function tipMath() {
 var Bill = document.getElementById("total").value //gets element by id
 var Tip = Bill * 0.15 // bill multiplied by 0.15 cents which is considered the starting point for dining 
 var Total = parseInt(Bill) + parseInt(Tip) // gathers bill and tip input
-console.log("Tip Amount: $ " + tip + " Total: $ " + Total ) //logs values to the console
+console.log("Tip Amount: $ " + Tip + " Total: $ " + Total ) //logs values to the console
 }
 
 //end 
