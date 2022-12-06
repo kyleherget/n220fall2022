81a92ba1ca095fe70153e57460f7b16f48c5721d
kyleherget <111830966+kyleherget@users.noreply.github.com>
2022-12-06 16:01:13 -0500

    update

--- FinalProject/LabFINAL/js/app.js
+++ FinalProject/LabFINAL/js/app.js
@@ -1,25 +1,56 @@
+(function(){
+
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let equal = document.querySelector(".btn-equal");
+  let clear = document.querySelector(".btn-clear");
+  let CE = document.querySelector('.btn-CE'); // delete one entry place from the right
+  let root = document.querySelector('.btn-root'); // evalaute a root 
+  let percent = document.querySelector('.btn-percent'); // evaule a percent
+
+
+  // functions for all buttons 
+  buttons.forEach(function(button) { // each button
+      button.addEventListener("click", function(e){ // listens for clicks on buttons and event has to be active 
+          let value = e.target.dataset.num; // assigns value from data-num when event is active 
+          screen.value += value; // adds value to the right of the screen
+      })
+  });
+  
+  // equal function 
+  equal.addEventListener("click", function(e){ // listens for click on = button 
+     if (screen.value === "") { // if - else loop to evaule the input and the value to the right of the last value
+        screen.value = ""; // clears 
+      } else{
+          let answer = eval(screen.value); // evluates with function 
+          screen.value = answer; // puts answer on the screen. 
+
+      } 
+  })
+
+ // clear function
+   clear.addEventListener("click", function(){ // listens for clicks on the C button 
+      screen.value = ""; // if event is active set screen value to ""
+      })
+
+      
+   CE.getElementsByClassName("btn-CE", function(){
+      screen.value = screen.value.splice(0,-1)
+
+    })
+
+
+})();
 
 
 
-(function() {
 
-    let screen = document.querySelector('.screen'); // display
-    let buttons = document.querySelectorAll('.btn'); // numbers
-    let clear = document.querySelector('.btn-clear'); // clear entry
-    let equal = document.querySelector('.btn-equal'); // evaluate entry 
-    let CE = document.querySelector('.btn-CE'); // delete one entry place from the right
-    let root = document.querySelector('.btn-root'); // evalaute a root 
-    let percent = document.querySelector('.btn-percent'); // evaule a percent 
 
-        //functions for buttons gets from dataset.num
-        buttons.forEach(function(button){ 
-            button.addEventListener('click', function(e){
-            let value = e.target.dataset.num;
-            screen.value += value;
-         })
-})
 
-//work on delay
+
+
+/*
+//plays sound
 let audio = new Audio("js/click.wav");
 let allbtns = document.querySelectorAll("button");
 
@@ -29,59 +60,8 @@ buttons.forEach(button => {
   });
 });
 
-
-
-
-
-function blinker() {
-
-        if ($('input[type=text]').attr('0')) {
-          // get the placeholder text
-          $('input[type=text]').attr('0', '');
-        } else {
-          $('input[type=text]').attr('0', '0');
-        }
-      
-        setTimeout(blinker, 1000);
-      
-}
-      
-//equal is not working for some reason 
-    equal.addEventListener('click', function(e) {
-        if (screen.value === "") {
-            
-        }
-
-        else {
-            let answer = eval(screen.value);
-            screen.value = answer;
-        }
-    })
-
-    // clear btn works
-    clear.addEventListener('click', function(e){
-        screen.value = "";
-    })
-
-   
-    
-        
-
-    
-
-})();
-
-
-
 // add square root function
 // add CE funtion --- working on 
 // add percetn function
 
-
- //fix splice CE function
-//CE.forEach(function(button){ 
-   // CE.addEventListener('click', function(e){
-    // screen.value = screen.value.splice(0,-1)
-    //})})
-
-   
+*/
